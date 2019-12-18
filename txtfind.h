8fada4a84facf86ab79ff0415cac656c1acd4f86
ohadcohen1111 <57725202+ohadcohen1111@users.noreply.github.com>
2019-12-18 11:53:43 +0200

    Create txtfind.h

---
+++ txtfind.h
@@ -0,0 +1,10 @@
+#define LINE 256
+#define WORD 30
+
+int Getline(char s[]);
+int Getword(char w[]);
+int stringLength(char * str2);
+int substring(char * str1, char * str2);
+int similar(char * s, char * t, int n);
+void print_lines(char*str);
+void print_similar_words(char * str);
